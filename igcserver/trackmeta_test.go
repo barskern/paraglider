234fd4a00744f64fb85eb1fe1d7adc740e7ebf22
Ole Martin Ruud <barskern@outlook.com>
2018-10-29 22:15:08 +0100

    Reorganize tests

--- igcserver/trackmeta_test.go
+++ igcserver/trackmeta_test.go
@@ -6,52 +6,6 @@ import (
 	"testing"
 )
 
-// TrackMetasMap contains a map to many TrackMeta objects which are protected
-// by a RWMutex and indexed by a unique id
-type TrackMetasMap struct {
-	sync.RWMutex
-	data map[TrackID]TrackMeta
-}
-
-// NewTrackMetasMap creates a new mutex and mapping from ID to TrackMeta
-func NewTrackMetasMap() TrackMetasMap {
-	return TrackMetasMap{sync.RWMutex{}, make(map[TrackID]TrackMeta)}
-}
-
-// Get fetches the track meta of a specific id if it exists
-func (metas *TrackMetasMap) Get(id TrackID) (meta TrackMeta, err error) {
-	metas.RLock()
-	defer metas.RUnlock()
-	meta, ok := metas.data[id]
-	if !ok {
-		err = ErrTrackNotFound
-	}
-	return
-}
-
-// Append appends a track meta and returns the given id
-func (metas *TrackMetasMap) Append(meta TrackMeta) (err error) {
-	metas.Lock()
-	defer metas.Unlock()
-	if _, exists := metas.data[meta.ID]; exists {
-		err = ErrTrackAlreadyExists
-	} else {
-		metas.data[meta.ID] = meta
-	}
-	return
-}
-
-// GetAllIDs fetches all the stored ids
-func (metas *TrackMetasMap) GetAllIDs() (ids []TrackID, err error) {
-	metas.RLock()
-	defer metas.RUnlock()
-	ids = make([]TrackID, 0, len(metas.data))
-	for id := range metas.data {
-		ids = append(ids, id)
-	}
-	return
-}
-
 // Test that all returned ids from 'Append' are found when using 'Get'
 func TestTrackMetaDuplicate(t *testing.T) {
 	meta := TrackMeta{
@@ -176,4 +130,50 @@ func TestTrackMetasGetAllIDs(t *testing.T) {
 			t.Fatalf("didn't find id '%d' in result of 'GetAllIDs'", pureID)
 		}
 	}
+}
+
+// TrackMetasMap contains a map to many TrackMeta objects which are protected
+// by a RWMutex and indexed by a unique id
+type TrackMetasMap struct {
+	sync.RWMutex
+	data map[TrackID]TrackMeta
+}
+
+// NewTrackMetasMap creates a new mutex and mapping from ID to TrackMeta
+func NewTrackMetasMap() TrackMetasMap {
+	return TrackMetasMap{sync.RWMutex{}, make(map[TrackID]TrackMeta)}
+}
+
+// Get fetches the track meta of a specific id if it exists
+func (metas *TrackMetasMap) Get(id TrackID) (meta TrackMeta, err error) {
+	metas.RLock()
+	defer metas.RUnlock()
+	meta, ok := metas.data[id]
+	if !ok {
+		err = ErrTrackNotFound
+	}
+	return
+}
+
+// Append appends a track meta and returns the given id
+func (metas *TrackMetasMap) Append(meta TrackMeta) (err error) {
+	metas.Lock()
+	defer metas.Unlock()
+	if _, exists := metas.data[meta.ID]; exists {
+		err = ErrTrackAlreadyExists
+	} else {
+		metas.data[meta.ID] = meta
+	}
+	return
+}
+
+// GetAllIDs fetches all the stored ids
+func (metas *TrackMetasMap) GetAllIDs() (ids []TrackID, err error) {
+	metas.RLock()
+	defer metas.RUnlock()
+	ids = make([]TrackID, 0, len(metas.data))
+	for id := range metas.data {
+		ids = append(ids, id)
+	}
+	return
 }
